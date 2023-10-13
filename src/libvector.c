3426d4ffe96018e5df1dd82bdff234f5d6b2221f
simonepenitozzi <147860785+simonepenitozzi@users.noreply.github.com>
2023-10-13 19:03:08 +0200

    Update libvector.c

--- src/libvector.c
+++ src/libvector.c
@@ -1,283 +1,301 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
+#include <string.h>
 #include <math.h>
 #include <stdarg.h>
 #include "libvector.h"
 
-/* Create a zero vector and return pointer to it */
-vector_t create_zero_vector(const unsigned int dimension)
-{
-	if(!dimension)
-		return NULL;
+// Define a struct for vectors
+typedef struct {
+    unsigned int dimension;
+    double *components;
+} Vector;
 
-	vector_t vector = (vector_t)malloc(sizeof(struct Vector));
-	vector->dimension = dimension;
-	vector->components = (double *)calloc(dimension, sizeof(double));
+// Define a typedef for the struct
+typedef Vector* vector_t;
 
-	return vector;
+// Function to create a zero vector
+vector_t create_zero_vector(const unsigned int dimension) {
+    if (dimension == 0) {
+        return NULL;
+    }
+
+    vector_t vector = (vector_t)malloc(sizeof(Vector));
+    vector->dimension = dimension;
+    vector->components = (double *)calloc(dimension, sizeof(double));
+
+    return vector;
 }
 
-/* Create a vector and return pointer to it */
-vector_t create_vector(const unsigned int dimension, const double components[])
-{
-	if(!dimension)
-		return NULL;
+// Function to create a vector from components
+vector_t create_vector(const unsigned int dimension, const double components[]) {
+    if (dimension == 0) {
+        return NULL;
+    }
 
-	vector_t vector = create_zero_vector(dimension);
-	memcpy(vector->components, components, vector->dimension * sizeof(double));
+    vector_t vector = create_zero_vector(dimension);
+    memcpy(vector->components, components, dimension * sizeof(double));
 
-	return vector;
+    return vector;
 }
 
-/* Delete a vector and free memory */
-void destroy_vector(vector_t *vector)
-{
-	if(*vector == NULL)
-		return;
-
-	free((*vector)->components);
-	free(*vector);
-	*vector = NULL;
+// Function to destroy a vector
+void destroy_vector(vector_t *vector) {
+    if (*vector != NULL) {
+        free((*vector)->components);
+        free(*vector);
+        *vector = NULL;
+    }
 }
 
-/* Delete the vectors passed by argument */
-void destroy_vectors(int numargs, ...)
-{
-	va_list list;
-	va_start(list, numargs);
-	for(int i = 0; i < numargs; i++)
-	{
-		vector_t *arg = va_arg(list, vector_t *);
-		destroy_vector(arg);
-	}
-	va_end(list);
+// Function to destroy multiple vectors
+void destroy_vectors(int numargs, ...) {
+    va_list list;
+    va_start(list, numargs);
+    for (int i = 0; i < numargs; i++) {
+        vector_t *arg = va_arg(list, vector_t *);
+        destroy_vector(arg);
+    }
+    va_end(list);
 }
 
-/* Print the components of a vector using custom format specifier */
-void printf_vector(const vector_t vector, const char *format)
-{
-	if(vector == NULL)
-		return;
+// Function to print a vector with a custom format
+void printf_vector(const vector_t vector, const char *format) {
+    if (vector == NULL) {
+        return;
+    }
 
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		printf(format, vector->components[i]);
-
-	puts("");
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        printf(format, vector->components[i]);
+    }
+    puts("");
 }
 
-/* Copy a vector */
-vector_t copy_vector(const vector_t vector)
-{
-	if(vector == NULL)
-		return NULL;
+// Function to create a copy of a vector
+vector_t copy_vector(const vector_t vector) {
+    if (vector == NULL) {
+        return NULL;
+    }
 
-	vector_t result = create_zero_vector(vector->dimension);
-	memcpy(result->components, vector->components, result->dimension * sizeof(double));
+    vector_t result = create_zero_vector(vector->dimension);
+    memcpy(result->components, vector->components, vector->dimension * sizeof(double));
 
-	return result;
+    return result;
 }
 
-/* Fill all components of a vector with a new value */
-void fill_vector(const vector_t vector, const double new_value)
-{
-	if(vector == NULL)
-		return;
+// Function to fill all components of a vector with a new value
+void fill_vector(const vector_t vector, const double new_value) {
+    if (vector == NULL) {
+        return;
+    }
 
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		vector->components[i] = new_value;
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        vector->components[i] = new_value;
+    }
 }
 
-/* Increase vector dimension and push a new element in the selected position */
-void vector_push(const vector_t vector, const double new_value, const unsigned int position)
-{
-	if(vector == NULL || position > vector->dimension)
-		return;
+// Function to push a new element at the specified position
+void vector_push(vector_t vector, const double new_value, const unsigned int position) {
+    if (vector == NULL || position > vector->dimension) {
+        return;
+    }
 
-	vector->components = (double *)realloc(vector->components, (vector->dimension + 1) * sizeof(double));
-	memmove(vector->components + position + 1, vector->components + position, (vector->dimension - position) * sizeof(double));
-	vector->components[position] = new_value;
-	(vector->dimension)++;
+    vector->dimension++;
+    vector->components = (double *)realloc(vector->components, vector->dimension * sizeof(double));
+
+    memmove(vector->components + position + 1, vector->components + position, (vector->dimension - position - 1) * sizeof(double));
+    vector->components[position] = new_value;
 }
 
-/* Decrease vector dimension and pop a element from the selected position */
-void vector_pop(const vector_t vector, const unsigned int position)
-{
-	if(vector == NULL || position > vector->dimension - 1)
-		return;
+// Function to pop an element from the specified position
+void vector_pop(vector_t vector, const unsigned int position) {
+    if (vector == NULL || position >= vector->dimension) {
+        return;
+    }
 
-	memmove(vector->components + position, vector->components + position + 1, (vector->dimension - position - 1) * sizeof(double));
-	vector->components = (double *)realloc(vector->components, (vector->dimension - 1) * sizeof(double));
-	(vector->dimension)--;
+    memmove(vector->components + position, vector->components + position + 1, (vector->dimension - position - 1) * sizeof(double));
+    vector->dimension--;
+    vector->components = (double *)realloc(vector->components, vector->dimension * sizeof(double));
 }
 
-/* Return the sum of two vectors */
-vector_t sum_vector(const vector_t vector1, const vector_t vector2)
-{
-	if(vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension)
-		return NULL;
+// Function to return the sum of two vectors
+vector_t sum_vector(const vector_t vector1, const vector_t vector2) {
+    if (vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension) {
+        return NULL;
+    }
 
-	vector_t vector = create_zero_vector(vector1->dimension);
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		vector->components[i] = vector1->components[i] + vector2->components[i];
+    vector_t vector = create_zero_vector(vector1->dimension);
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        vector->components[i] = vector1->components[i] + vector2->components[i];
+    }
 
-	return vector;
+    return vector;
 }
 
-/* Calculate the dot product between two vectors */
-double dot_product(const vector_t vector1, const vector_t vector2)
-{
-	// Return error ?
-	/*if(vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension)
-		return;*/
+// Function to calculate the dot product between two vectors
+double dot_product(const vector_t vector1, const vector_t vector2) {
+    if (vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension) {
+        return 0.0; // Consider returning a default value or indicating an error
+    }
 
-	double sum = 0.0;
-	for(unsigned int i = 0; i < vector1->dimension; i++)
-		sum += vector1->components[i] * vector2->components[i];
+    double sum = 0.0;
+    for (unsigned int i = 0; i < vector1->dimension; i++) {
+        sum += vector1->components[i] * vector2->components[i];
+    }
 
-	return sum;
+    return sum;
 }
 
-/* Calculate the cross product between two vectors */
-vector_t cross_product(const vector_t vector1, const vector_t vector2)
-{
-	if(vector1 == NULL || vector2 == NULL || vector1->dimension != 3 || vector2->dimension != 3) // cross_product is defined in R^3
-		return NULL;
+// Function to calculate the cross product between two vectors in R^3
+vector_t cross_product(const vector_t vector1, const vector_t vector2) {
+    if (vector1 == NULL || vector2 == NULL || vector1->dimension != 3 || vector2->dimension != 3) {
+        return NULL;
+    }
 
-	vector_t vector = create_zero_vector(3);
-	vector->components[0] = vector1->components[1] * vector2->components[2] - vector1->components[2] * vector2->components[1];
-	vector->components[1] = vector1->components[2] * vector2->components[0] - vector1->components[0] * vector2->components[2];
-	vector->components[2] = vector1->components[0] * vector2->components[1] - vector1->components[1] * vector2->components[0];
+    vector_t vector = create_zero_vector(3);
+    vector->components[0] = vector1->components[1] * vector2->components[2] - vector1->components[2] * vector2->components[1];
+    vector->components[1] = vector1->components[2] * vector2->components[0] - vector1->components[0] * vector2->components[2];
+    vector->components[2] = vector1->components[0] * vector2->components[1] - vector1->components[1] * vector2->components[0];
 
-	return vector;
+    return vector;
 }
 
-/* Calculate the product beetween a vector and a scalar */
-void vector_scalar_multiplication(const vector_t vector, const double scalar)
-{
-	if(vector == NULL)
-		return;
+// Function to multiply a vector by a scalar
+void vector_scalar_multiplication(vector_t vector, const double scalar) {
+    if (vector == NULL) {
+        return;
+    }
 
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		vector->components[i] *= scalar;
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        vector->components[i] *= scalar;
+    }
 }
 
-/* Add a scalar to vector components */
-void vector_add_scalar(const vector_t vector, const double scalar)
-{
-	if(vector == NULL)
-		return;
+// Function to add a scalar to vector components
+void vector_add_scalar(vector_t vector, const double scalar) {
+    if (vector == NULL) {
+        return;
+    }
 
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		vector->components[i] += scalar;
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        vector->components[i] += scalar;
+    }
 }
 
-/* Calculate the absolute value of vector components */
-void vector_abs(const vector_t vector)
-{
-	if(vector == NULL)
-		return;
+// Function to calculate the absolute value of vector components
+void vector_abs(vector_t vector) {
+    if (vector == NULL) {
+        return;
+    }
 
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		if(vector->components[i] < 0)
-			vector->components[i] *= -1;
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        if (vector->components[i] < 0) {
+            vector->components[i] *= -1;
+        }
+    }
 }
 
-/* Calculate the magnitude of a vector */
-double vector_magnitude(const vector_t vector)
-{
-	// return error ?
-	/*if(vector == NULL)
-		return;*/
+// Function to calculate the magnitude of a vector
+double vector_magnitude(const vector_t vector) {
+    if (vector == NULL) {
+        return 0.0; // Consider returning a default value or indicating an error
+    }
 
-	return sqrt(dot_product(vector, vector));
+    return sqrt(dot_product(vector, vector));
 }
 
-/* Calculate the normalized vector */
-void vector_normalize(const vector_t vector)
-{
-	if(vector == NULL)
-		return;
+// Function to normalize a vector
+void vector_normalize(vector_t vector) {
+    if (vector == NULL) {
+        return;
+    }
 
-	const double magnitude = vector_magnitude(vector);
-	
-	if(magnitude)
-		for(unsigned int i = 0; i < vector->dimension; i++)
-			vector->components[i] /= magnitude;
+    double magnitude = vector_magnitude(vector);
+    if (magnitude != 0) {
+        for (unsigned int i = 0; i < vector->dimension; i++) {
+            vector->components[i] /= magnitude;
+        }
+    }
 }
 
-/* Calculate the angle in radians between two vectors */
-double vector_angle(const vector_t vector1, const vector_t vector2)
-{
-	// Return error ?
-	/*if(vector1 == NULL || vector2 == NULL)
-		return;*/
+// Function to calculate the angle in radians between two vectors
+double vector_angle(const vector_t vector1, const vector_t vector2) {
+    if (vector1 == NULL || vector2 == NULL) {
+        return 0.0; // Consider returning a default value or indicating an error
+    }
 
-	double dp = fabs(dot_product(vector1, vector2));
-	double lenght1 = vector_magnitude(vector1);
-	double lenght2 = vector_magnitude(vector2);
-	return acos(dp / (lenght1 * lenght2));
+    double dp = fabs(dot_product(vector1, vector2));
+    double length1 = vector_magnitude(vector1);
+    double length2 = vector_magnitude(vector2);
+
+    return acos(dp / (length1 * length2));
 }
 
-/* Return the max component index from a vector */
-int max_vector_component(const vector_t vector)
-{
-	if(vector == NULL)
-		return -1;
+// Function to return the index of the maximum component in a vector
+int max_vector_component(const vector_t vector) {
+    if (vector == NULL) {
+        return -1; // Consider returning a default value or indicating an error
+    }
 
-	unsigned int max_index = 0;
-	for(unsigned int i = 1; i < vector->dimension; i++)
-		if(vector->components[i] > vector->components[max_index])
-			max_index = i;
+    unsigned int max_index = 0;
+    for (unsigned int i = 1; i < vector->dimension; i++) {
+        if (vector->components[i] > vector->components[max_index]) {
+            max_index = i;
+        }
+    }
 
-	return max_index;
+    return max_index;
 }
 
-/* Return the min component index from a vector */
-int min_vector_component(const vector_t vector)
-{
-	if(vector == NULL)
-		return -1;
+// Function to return the index of the minimum component in a vector
+int min_vector_component(const vector_t vector) {
+    if (vector == NULL) {
+        return -1; // Consider returning a default value or indicating an error
+    }
 
-	unsigned int min_index = 0;
-	for(unsigned int i = 1; i < vector->dimension; i++)
-		if(vector->components[i] < vector->components[min_index])
-			min_index = i;
+    unsigned int min_index = 0;
+    for (unsigned int i = 1; i < vector->dimension; i++) {
+        if (vector->components[i] < vector->components[min_index]) {
+            min_index = i;
+        }
+    }
 
-	return min_index;
+    return min_index;
 }
 
-/* Check if a vector is a zero vector */
-int is_zero_vector(const vector_t vector)
-{
-	if(vector == NULL)
-		return 0;
+// Function to check if a vector is a zero vector
+int is_zero_vector(const vector_t vector) {
+    if (vector == NULL) {
+        return 0;
+    }
 
-	for(unsigned int i = 0; i < vector->dimension; i++)
-		if(vector->components[i] != 0)
-			return 0;
+    for (unsigned int i = 0; i < vector->dimension; i++) {
+        if (vector->components[i] != 0) {
+            return 0;
+        }
+    }
 
-	return 1;
+    return 1;
 }
 
-/* Check if two vectors are parallel */
-int is_parallel_to(const vector_t vector1, const vector_t vector2)
-{
-	if(vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension)
-		return 0;
+// Function to check if two vectors are parallel
+int is_parallel_to(const vector_t vector1, const vector_t vector2) {
+    if (vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension) {
+        return 0;
+    }
 
-	vector_t vector = cross_product(vector1, vector2);
-	int result = is_zero_vector(vector);
-	destroy_vector(&vector);
+    vector_t cross = cross_product(vector1, vector2);
+    int result = is_zero_vector(cross);
+    destroy_vector(&cross);
 
-	return result;
+    return result;
 }
 
-/* Check if two vectors are perpendicular */
-int is_perpendicular_to(const vector_t vector1, const vector_t vector2)
-{
-	if(vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension)
-		return 0;
+// Function to check if two vectors are perpendicular
+int is_perpendicular_to(const vector_t vector1, const vector_t vector2) {
+    if (vector1 == NULL || vector2 == NULL || vector1->dimension != vector2->dimension) {
+        return 0;
+    }
 
-	return !dot_product(vector1, vector2);	
+    return (dot_product(vector1, vector2) == 0);
 }
